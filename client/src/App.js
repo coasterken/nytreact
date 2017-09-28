d50b0d8818f14d7ce41aad4701a331ace1dd0b09
coasterken <ken.r.thompson@icloud.com>
2017-09-27 22:17:07 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Articles from "./pages/Articles";
 
 const App = () => (
+  <Router>
   <div>
-    <Articles />
+    <Route exact path="/" component={Articles} />
+    <Route exact path="/articles" component={Articles} />
+    <Route exact path="/articles/:id" component={Articles} />
+    {/* <Articles /> */}
   </div>
+  </Router>
 );
 
 
