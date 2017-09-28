d467332eae1b4fcbdd9709b8e0ed95cb816ec73b
coasterken <ken.r.thompson@icloud.com>
2017-09-27 22:39:13 -0400

    update

--- client/src/pages/Articles/ArticlesBackup.js
+++ client/src/pages/Articles/ArticlesBackup.js
@@ -131,6 +131,7 @@ class Articles extends Component {
         {/* Results */}
         <Row>
             <Col size="md-12">
+             <br />
              <div className="panel panel-primary">
                <div className="panel-heading">
                   <h3 className="panel-title"><strong><i className="fa fa-table"></i>  Top Articles</strong></h3>
@@ -162,7 +163,7 @@ class Articles extends Component {
         {/* Saved Articles */}
         <Row>
           <Col size="md-12">
-            
+            <br />
             <div className="panel panel-primary">
               <div className="panel-heading">
                 <h3 className="panel-title"><strong><i className="fa fa-table"></i>  Saved Articles</strong></h3>
